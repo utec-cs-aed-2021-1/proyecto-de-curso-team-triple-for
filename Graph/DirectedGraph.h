6c1035cc0705ba4b49eb43aa3cba9698ced8143a
morphisjustfun <mariojacobo2011@gmail.com>
2021-06-27 11:54:06 -0500

    feat(deleteVertex): implement method
    fix(counter): add counter updates
    
    style(codebase): implement clang style rules

--- Graph/DirectedGraph.h
+++ Graph/DirectedGraph.h
@@ -2,120 +2,119 @@
 #define NONDIRECTEDGRAPH_H
 
 #include "graph.h"
+#include <iostream>
 
-template<typename TV, typename TE>
-class DirectedGraph : public Graph<TV, TE>{
+template <typename TV, typename TE> class DirectedGraph : public Graph<TV, TE> {
 public:
+  DirectedGraph() = default;
 
-    DirectedGraph() = default;
+  bool insertVertex(string id, TV vertex) override {
+    auto *newVertex = new Vertex<TV, TE>;
+    newVertex->data = vertex;
+    if (this->vertexes.find(id) != this->vertexes.end()) {
+      return false;
+    } else {
+      this->vertexes[id] = newVertex;
+      this->count_vertex++;
+      return true;
+    }
+  }
+  bool createEdge(string id1, string id2, TE w) override {
+    if (this->vertexes.find(id1) != this->vertexes.end() &&
+        this->vertexes.find(id2) != this->vertexes.end()) {
+      auto *new_edge = new Edge<TV, TE>;
+      new_edge->weight = w;
+      new_edge->vertexes[0] = this->vertexes[id1];
+      new_edge->vertexes[1] = this->vertexes[id2];
 
-    bool insertVertex(string id, TV vertex) override{
-        auto* newVertex = new Vertex<TV, TE>;
-        newVertex->data = vertex;
-        if (this->vertexes.find(id) != this->vertexes.end()) {
-            return false;
-        } else {
-            this->vertexes[id] = newVertex;
-            this->count_vertex++;
-            return true;
+      this->vertexes[id1]->edges.push_front(new_edge);
+
+      this->count_edge++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  bool deleteVertex(string id) override { 
+    if (this->vertexes.find(id) == this->vertexes.end()) {
+      return false;
+    } else {
+      for (auto &vertexes : this->vertexes) {
+        for (auto &edge : vertexes.second->edges) {
+          if (edge->vertexes[0] == this->vertexes[id]) {
+            vertexes.second->edges.remove(edge);
+            edge = nullptr;
+            delete edge;
+            break;
+          }
+          if (edge->vertexes[1] == this->vertexes[id]) {
+            vertexes.second->edges.remove(edge);
+            edge = nullptr;
+            delete edge;
+            break;
+          }
         }
+      }
+      this->vertexes[id]->edges.clear();
+      delete this->vertexes[id];
+      this->vertexes.erase(id);
+      this->count_vertex--;
+      return true;
     }
-    bool createEdge(string id1, string id2, TE w) override{
-        if(this->vertexes.find(id1) != this->vertexes.end() && this->vertexes.find(id2)  != this->vertexes.end()){
-            auto* new_edge = new Edge<TV, TE>;
-            new_edge->weight = w;
-            new_edge->vertexes[0] = this->vertexes[id1];
-            new_edge->vertexes[1] = this->vertexes[id2];
-
-            this->vertexes[id1]->edges.push_front(new_edge);
-
-            this->count_edge++;
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    bool deleteVertex(string id) override{ ////////faltaaaa
-        if (this->vertexes.find(id) == this->vertexes.end()) {
-            return false;
-        } else {
-            for (auto it: this->vertexes[id]->edges){
-
-            }
-            this->vertexes.erase(id);
-            return true;
-        }
-        /*for(auto map : this->vertexes) {
-            if(map.first != id){
-                return false;
-            } else{
-                for (auto const& i : map.second->edges) {
-                    if(i->vertexes[1] == &this->vertexes.at(id)){
-                        map.second->edges.remove(i);
-                        this->count_edge--;
-                    }
-                }
-            }
-        }
-        this->vertexes.erase(id);
-        this->count_vertex--;
-        return true;*/
-    }
-    bool deleteEdge(string start, string end) override{
-        if (this->vertexes.find(start) == this->vertexes.end() && this->vertexes.find(start) == this->vertexes.end()) {
-            cout << "GA\n";
-            return false;
-        }
-
-        for (auto edge: this->vertexes[start]->edges) {
-            if (edge->vertexes[0] == this->vertexes[start] && edge->vertexes[1] == this->vertexes[end]){
-                this->vertexes[start]->edges.remove(edge);
-                this->vertexes[end]->edges.remove(edge);
-                break;
-            }
-        }
-        return true;
+  }
+  bool deleteEdge(string start, string end) override {
+    if (this->vertexes.find(start) == this->vertexes.end() &&
+        this->vertexes.find(start) == this->vertexes.end()) {
+      return false;
     }
 
-    TE &operator()(string start, string end) {
-
+    for (auto edge : this->vertexes[start]->edges) {
+      if (edge->vertexes[0] == this->vertexes[start] &&
+          edge->vertexes[1] == this->vertexes[end]) {
+        this->vertexes[start]->edges.remove(edge);
+        this->vertexes[end]->edges.remove(edge);
+        break;
+      }
     }
+    this->count_edge--;
+    return true;
+  }
 
-    float density() override{
-        return (float)(this->count_edge)/(this->count_vertex * (this->count_vertex - 1));
-    }
+  TE &operator()(string start, string end) {}
 
-    bool isDense(float threshold = 0.5) override{
-        if((float)(this->count_edge)/(this->count_vertex * (this->count_vertex - 1))< threshold) return true;
-        return false;
-    }
+  float density() override {
+    return (float)(this->count_edge) /
+           (this->count_vertex * (this->count_vertex - 1));
+  }
 
-    bool isConnected() {
+  bool isDense(float threshold = 0.5) override {
+    if ((float)(this->count_edge) /
+            (this->count_vertex * (this->count_vertex - 1)) <
+        threshold)
+      return true;
+    return false;
+  }
 
-    }
+  bool isConnected() {}
 
-    bool isStronglyConnected() throw(){
+  bool isStronglyConnected() throw() {}
 
+  void displayVertex(string id) override {
+    cout << id << "-" << this->vertexes.at(id)->data << endl;
+  }
+  bool findById(string id) override { return this->vertexes.at(id); }
+  void display() override {
+    cout << "DISPLAY: \n";
+    for (auto &it : this->vertexes) {
+      cout << "Id: " << it.first << " value: " << it.second->data << endl;
+      cout << "Edges :" << endl;
+      for (auto const &i : it.second->edges) {
+        cout << "   {" << i->vertexes[0]->data << ", " << i->vertexes[1]->data
+             << "} w: " << i->weight << endl;
+      }
+      std::cout << std::endl;
     }
-
-    void displayVertex(string id) override{
-        cout<<id<<"-"<<this->vertexes.at(id)->data<<endl;
-    }
-    bool findById(string id) override{
-        return this->vertexes.at(id);
-    }
-    void display() override{
-        cout << "DISPLAY: \n";
-        for (auto& it: this->vertexes) {
-            cout << "Id: " << it.first << " value: " << it.second->data << endl;
-            cout << "Edges :" <<endl;
-            for (auto const& i : it.second->edges) {
-                cout << "   {"<<i->vertexes[0]->data << ", " << i->vertexes[1]->data << "} w: " << i->weight <<endl;
-            }
-            std::cout << std::endl;
-        }
-    }
+  }
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,65 +2,64 @@
 
 //#include "Tester/tester.h"
 //#include "Parser/parser.h"
-#include "UndirectedGraph.h"
-#include "DirectedGraph.h"
+#include "Graph/DirectedGraph.h"
+#include "Graph/UndirectedGraph.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    std::cout << "================================================" << std::endl;
-    std::cout << "MENU UNDIRECT GRAPH TESTER" << std::endl;
-    std::cout << "================================================" << std::endl;
-    
-    //Tester::executeExamples();
-    //Tester::executeParser();
+  std::cout << "================================================" << std::endl;
+  std::cout << "MENU UNDIRECT GRAPH TESTER" << std::endl;
+  std::cout << "================================================" << std::endl;
 
-    auto* g = new UnDirectedGraph<int, int>();
-    g->insertVertex("a", 1);
-    g->insertVertex("b", 2);
-    g->insertVertex("c", 3);
-    g->insertVertex("d", 4);
+  // Tester::executeExamples();
+  // Tester::executeParser();
 
-    g->createEdge("a", "b", 2);
-    g->createEdge("a", "d", 5);
+  // auto *g = new UnDirectedGraph<int, int>();
+  // g->insertVertex("a", 1);
+  // g->insertVertex("b", 2);
+  // g->insertVertex("c", 3);
+  // g->insertVertex("d", 4);
+  //
+  // g->createEdge("a", "b", 2);
+  // g->createEdge("a", "d", 5);
+  //
+  // g->display();
+  //
+  // g->deleteVertex("a");
+  // // g->deleteEdge("a", "b");
+  //
+  // std::cout << std::endl;
+  // std::cout << std::endl;
+  //
+  // g->display();
+  //
+  // std::cout << std::endl;
+  // std::cout << std::endl;
+  // std::cout << std::endl;
 
-    g->display();
+  std::cout << "================================================" << std::endl;
+  std::cout << "MENU DIRECT GRAPH TESTER" << std::endl;
+  std::cout << "================================================" << std::endl;
 
-    //g->deleteVertex("a");
-    g->deleteEdge("a", "b");
+  auto *d = new DirectedGraph<int, int>();
+  d->insertVertex("a", 1);
+  d->insertVertex("b", 2);
+  d->insertVertex("c", 3);
 
-    std::cout << std::endl;
-    std::cout << std::endl;
+  d->createEdge("a", "b", 2);
+  d->createEdge("b", "a", 6);
 
-    g->display();
+  d->display();
 
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << std::endl;
+  d->deleteVertex("a");
+  d->deleteEdge("b", "a");
+  std::cout << std::endl;
+  std::cout << std::endl;
+  cout << d->density() << endl;
+  cout << d->isDense() << endl;
 
-    std::cout << "================================================" << std::endl;
-    std::cout << "MENU DIRECT GRAPH TESTER" << std::endl;
-    std::cout << "================================================" << std::endl;
-
-    auto* d = new DirectedGraph<int, int>();
-    d->insertVertex("a", 1);
-    d->insertVertex("b", 2);
-    d->insertVertex("c", 3);
-
-    d->createEdge("a", "b", 2);
-    d->createEdge("b", "a", 6);
-
-    d->display();
-
-    //g->deleteVertex("a");
-
-    d->deleteEdge("b", "a");
-    std::cout << std::endl;
-    std::cout << std::endl;
-    cout<< d->density() <<endl;
-    cout<< d->isDense() <<endl;
-
-    d->display();
-    return EXIT_SUCCESS;
+  d->display();
+  return EXIT_SUCCESS;
 }
